0666c4ead2228a24362f4b1c9a590b36a8f8cc85
fridaytd <friday.infortech@gmail.com>
2023-04-04 12:31:48 +0700

    update st

--- src/Views/page/Home_page.php
+++ src/Views/page/Home_page.php
@@ -1,4 +1,4 @@
-<main>
+<main class="home">
     <div>
         <div id="carouselExampleIndicators" class="carousel slide my-1" data-bs-ride="true">
             <div class="carousel-indicators">
@@ -28,8 +28,44 @@
         </div>
     </div>
     <div class="container">
-        <div class="row">
-            <h1>Hihi</h1>
+        <div class="row justify-content-around my-5">
+            <div class="col-md-5 justify-content-center d-flex">
+                <img src=" /images/Home/bannertrangchu.jpg" alt="" width="100%">
+            </div>
+            <div class="col-md-5 d-flex justify-content-center align-items-center">
+                <div>
+                    <h4>TỪ NHỮNG MẦM TRÀ, CHÚNG TÔI TẠO RA NIỀM ĐAM MÊ</h4>
+                    <p style="text-align: justify">Trải qua hơn 50 năm chắt chiu tinh hoa từ những búp trà xanh và
+                        hạt cà phê thượng hạng
+                        cùng
+                        mong muốn mang lại cho khách hàng những trải nghiệm giá trị nhất khi thưởng thức, Phúc
+                        Long liên tục là thương hiệu tiên phong với nhiều ý tưởng sáng tạo đi đầu trong ngành
+                        trà
+                        và cà phê.
+
+                        Chúng tôi tin rằng từng sản phẩm trà và cà phê sẽ càng thêm hảo hạng khi được tạo ra
+                        từ
+                        sự phấn đấu không ngừng cùng niềm đam mê. Và chính kết nối dựa trên niềm tin, sự trung thực
+                        và
+                        tin yêu sẽ góp phần mang đến những nét đẹp trong văn hóa thưởng trà và cà phê ngày càng bay cao,
+                        vươn xa.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-around my-5">
+            <div class="col-md-5 d-flex justify-content-center align-items-center">
+                <div>
+                    <h4>ĐỘI NGŨ NHÂN VIÊN TRÀN ĐẦY NHIỆT HUYẾT</h4>
+                    <p style="text-align: justify">Trong suốt quá trình hoạt động và phát triển, đội ngũ quản lý
+                        và nhân viên của Phúc Long Coffee & Tea, qua bao thế hệ, đã cùng nhau xây dựng, nuôi dưỡng
+                        niềm đam mê dành cho trà và cà phê với mong muốn được thử thách bản thân trong ngành
+                        dịch vụ năng động và sáng tạo.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-5 justify-content-center d-flex">
+                <img src=" /images/Home/tuyendung1.jpg" alt="" width="70%">
+            </div>
         </div>
     </div>
 </main>
--- src/Views/template.php
+++ src/Views/template.php
@@ -6,15 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" media="all" href="<?= '/css/style.css' ?>">
-    <title>Document</title>
+    <title>
+        <?= $data['page'] ?>
+    </title>
 </head>
 
 <body>
+    <div id="header"></div>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="/Home"><img src="/images/logo.png" alt="Logo" width="70px"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
@@ -24,7 +28,7 @@
                 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
                         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
                             aria-label="Close"></button>
                     </div>
@@ -52,13 +56,14 @@
                             </li>
                         </ul>
                         <div>
-                            <button class="btn" style="background-color: #0C713D; color: white">Sign in</button>
-                            <button class="btn" style="border:2px solid #0C713D; color: #0C713D;">Sign up</button>
+                            <button class="btn" style="background-color: #0C713D; color: white">Đăng nhập</button>
+                            <button class="btn" style="border:2px solid #0C713D; color: #0C713D;">Đăng kí</button>
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
+        <a href="#header" id="return-to-top" class="btn"><i class="fas fa-chevron-up" style="color: #0e4c2f;"></i></a>
     </header>
 
     <?php if (file_exists('../src/Views/page/' . $data['page'] . '_page.php')) {
@@ -67,9 +72,38 @@
     }
     ?>
 
-    <footer>
-        <div class="container-fluid d-flex justify-content-center align-item-center">
-            <p>Copyright</p>
+    <footer class="mt-3">
+        <div class="top-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <p><b>Trụ sở chính:</b> Công ty Cổ Phần Phúc Long Heritage - ĐKKD: 0316 871719
+                            do sở KHĐT TPHCM cấp lần đầu ngày 21/05/2021</p>
+                        <p><b>Nhà máy:</b> D_8D_CN Đường XE 1, Khu Công Nghiệp Mỹ Phước III, phường Mỹ Phước, thị xã Bến
+                            Cát, tỉnh Bình Dương, Việt Nam</p>
+                        <p><b>Địa chỉ:</b> Phòng 702, Tầng 7, Tòa nhà Central Plaza, số 17 Lê Duẩn, phường Bến Nghé,
+                            quận
+                            1, Hồ Chí Minh</p>
+                        <p><b>Điện thoại:</b> 1900 234 518 (Ext.9100/ 9102)</p>
+                        <p><b>Email:</b> sales@phuclong.masangroup.com</p>
+                    </div>
+                    <div class="col-md-4 ps-5">
+                        <div><a href="">Chính sách đặt hàng</a></div>
+                        <div> <a href="">Chính sách bảo mật thông tin</a></div>
+
+                    </div>
+                    <div class="col-md-4">
+                        <div><img src="/images/dathongbao.png" alt="" width="50%"></div>
+                        <div><i class="fab fa-facebook-f" style="color: #ffffff;"></i> <i class="fab fa-twitter"
+                                style="color: #ffffff;"></i> <i class="fab fa-instagram" style="color: #ffffff;"></i>
+                            <i class="fab fa-youtube" style="color: #ffffff;"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bot-footer text-center">
+            Copyright by Dang & Tin
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
